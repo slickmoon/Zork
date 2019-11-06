c1a2b87499bd35d5499439e9293aee18eee99c67
Nicholas Cass <nickcass88@gmail.com>
2019-11-06 20:15:56 +1100

    Adding ZorkScores entity framework database

---
+++ ZorkSite/Scores.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Scores.aspx.cs" Inherits="ZorkSite.Highscores" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Hi</h1> 
+            <jeff>I am a jeff block</jeff>
+            <table class="error" id="errortable">
+<tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>94</td>
+  </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ZorkSite/Scores.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ZorkSite
+{
+    public partial class Highscores : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (ZorkScoresEntities db = new ZorkScoresEntities())
+            {
+                
+                Highscore art = db.Highscores.Where((x) => x.Name == "Nick").FirstOrDefault();
+
+                Response.Write(art.Score.ToString());
+
+            }
+
+            using (ZorkScoresEntities db = new ZorkScoresEntities())
+            {
+                var newscore = db.Set<Highscore>();
+                newscore.Add(new Highscore { Name = "Timmy", Score = 99, SubmitDateTime = DateTime.UtcNow });
+
+                db.SaveChanges();
+            }
+
+        }
+    }
+}
